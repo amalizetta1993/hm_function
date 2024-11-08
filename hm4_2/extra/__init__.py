aa50f8d6da76555336c86a6ef3c96e0c16d95cab
Aleksandra Kamchenkova <amalizetta@gmail.com>
2024-11-08 23:15:27 +0300

    add import functions

--- hm4_2/extra/__init__.py
+++
@@ -1,5 +0,0 @@
-# from sys import path
-# path.append(r'..\hm4_2')
-
-
-# from extra.folder1.folder2.folder3 import file6
---
+++ hm4_2/main.py
@@ -0,0 +1,18 @@
+from extra.folder1.file1 import func_file1
+from extra.folder1.file2 import func_file2
+from extra.folder1.folder2.file3 import func_file3
+from extra.folder1.folder2.file4 import func_file4
+from extra.folder1.folder2.folder3.file5 import func_file5
+from extra.folder1.folder2.folder3.file6 import func_file6
+from folder.file_folder import func_file_folder
+from folder.file_folder2 import func_file_folder2
+
+print(func_file1())
+print(func_file2())
+print(func_file3())
+print(func_file4())
+print(func_file5())
+print(func_file6())
+print(func_file_folder())
+print(func_file_folder2())
+
